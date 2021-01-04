034513bbbbe57bcfd55623d792ad8af5d89510dc
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-01-04 15:18:00 -0500

    Add files via upload

---
+++ vs/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.Tennesesse = new System.Windows.Forms.Timer(this.components);
+            this.Tony = new System.Windows.Forms.Timer(this.components);
+            this.Turk = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.Score = new System.Windows.Forms.Label();
+            this.scoree = new System.Windows.Forms.Label();
+            this.Teddy = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(73, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 110);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(666, 320);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 118);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox3.Location = new System.Drawing.Point(345, 182);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 29);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox4.Location = new System.Drawing.Point(-9, -16);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(863, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox5.Location = new System.Drawing.Point(-15, 434);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(863, 50);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Tennesesse
+            // 
+            this.Tennesesse.Enabled = true;
+            this.Tennesesse.Tick += new System.EventHandler(this.Tennesesse_Tick);
+            // 
+            // Tony
+            // 
+            this.Tony.Enabled = true;
+            this.Tony.Interval = 500;
+            this.Tony.Tick += new System.EventHandler(this.Tony_Tick);
+            // 
+            // Turk
+            // 
+            this.Turk.Enabled = true;
+            this.Turk.Interval = 500;
+            this.Turk.Tick += new System.EventHandler(this.Turk_Tick);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox6.Location = new System.Drawing.Point(-39, 22);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(56, 432);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox7.Location = new System.Drawing.Point(780, 6);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(41, 432);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Location = new System.Drawing.Point(40, 6);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(68, 20);
+            this.Score.TabIndex = 7;
+            this.Score.Text = "Score: 0";
+            this.Score.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // scoree
+            // 
+            this.scoree.AutoSize = true;
+            this.scoree.Location = new System.Drawing.Point(678, 6);
+            this.scoree.Name = "scoree";
+            this.scoree.Size = new System.Drawing.Size(68, 20);
+            this.scoree.TabIndex = 8;
+            this.scoree.Text = "Score: 0";
+            this.scoree.Click += new System.EventHandler(this.scoree_Click);
+            // 
+            // Teddy
+            // 
+            this.Teddy.Enabled = true;
+            this.Teddy.Tick += new System.EventHandler(this.Teddy_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scoree);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer Tennesesse;
+        private System.Windows.Forms.Timer Tony;
+        private System.Windows.Forms.Timer Turk;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label scoree;
+        private System.Windows.Forms.Timer Teddy;
+    }
+}
+
---
+++ vs/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        bool right = false;
+        bool left = true;
+        bool up = true;
+        bool down = false;
+        bool up2 = false;
+        bool down2 = true;
+        bool stop = false;
+        bool stop2 = false;
+        int tonyTime = 0;
+        int turkTime = 0;
+        int Score1 = 0;
+        int Score2 = 0;
+        bool hit = false;
+        bool hit2 = false;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs key)
+        {
+            if (key.KeyCode == Keys.Q)
+            {
+                stop = true;
+            }
+            if (key.KeyCode == Keys.P)
+            {
+                stop2 = true;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Tennesesse_Tick(object sender, EventArgs e)
+        {
+            if (stop == false) // code for first player
+            {
+                if (up == true)
+                {
+                    pictureBox1.Top -= 5;
+
+                    if (pictureBox1.Bounds.IntersectsWith(pictureBox4.Bounds) == true)
+                    {
+                        up = false;
+                        down = true;
+                    }
+                }
+                if (down == true)
+                {
+                    pictureBox1.Top += 5;
+
+                    if (pictureBox1.Bounds.IntersectsWith(pictureBox5.Bounds) == true)
+                    {
+                        down = false;
+                        up = true;
+                    }
+                }
+            }
+
+            if (stop2 == false) // code for second player
+            {
+                if (up2 == true) // if they are going up
+                {
+                    pictureBox2.Top -= 5;
+
+                    if (pictureBox2.Bounds.IntersectsWith(pictureBox4.Bounds) == true)
+                    {
+                        up2 = false;
+                        down2 = true;
+                    }
+                }
+                if (down2 == true) // if they are going down
+                {
+                    pictureBox2.Top += 5;
+
+                    if (pictureBox2.Bounds.IntersectsWith(pictureBox5.Bounds) == true)
+                    {
+                        down2 = false;
+                        up2 = true;
+                    }
+                }
+            }
+
+            if (left == true)
+            {
+                pictureBox3.Left -= 5;
+
+                if (pictureBox3.Bounds.IntersectsWith(pictureBox1.Bounds) == true)
+                {
+                    left = false;
+                    right = true;
+                }
+                if (pictureBox3.Bounds.IntersectsWith(pictureBox6.Bounds) == true)
+                {
+                    pictureBox3.Left += 300;
+                    Score1 += 1;
+                    hit = true;
+                }
+            }
+            if (right == true)
+            {
+                pictureBox3.Left += 5;
+
+                if (pictureBox3.Bounds.IntersectsWith(pictureBox2.Bounds) == true)
+                {
+                    left = true;
+                    right = false;
+                }
+                if (pictureBox3.Bounds.IntersectsWith(pictureBox7.Bounds) == true)
+                {
+                    pictureBox3.Left -= 300;
+                    Score2 += 1;
+                    hit2 = true;
+                }
+            }
+        }
+
+        private void Tony_Tick(object sender, EventArgs e)
+        {
+            if (stop == true)
+            {
+                tonyTime += 1;
+
+                if (tonyTime >= 5)
+                {
+                    stop = false;
+                    tonyTime = 0;
+                }
+            }
+        }
+
+        private void Turk_Tick(object sender, EventArgs e)
+        {
+            if (stop2 == true)
+            {
+                turkTime += 1;
+
+                if (turkTime >= 5)
+                {
+                    stop2 = false;
+                    turkTime = 0;
+                }
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (hit == true)
+            {
+                Score.Text = "Score: " + Score1.ToString();
+                hit = false;
+            }
+        }
+
+        private void scoree_Click(object sender, EventArgs e)
+        {
+            if (hit == true)
+            {
+                scoree.Text = "Score: " + Score2.ToString();
+                hit = false;
+            }
+        }
+
+        private void Teddy_Tick(object sender, EventArgs e)
+        {
+            if (hit2 == true)
+            {
+                Score.Text = "Score: " + Score2.ToString();
+                hit2 = false;
+
+                if (Score2 == 3)
+                {
+                    string msg = "Player 1 WIN!";
+                    MessageBox.Show(msg);
+                    Application.Exit();
+
+                }
+            }
+            if (hit == true)
+            {
+                scoree.Text = "Score: " + Score1.ToString();
+                hit = false;
+
+                if (Score1 == 3)
+                {
+                    string msgs = "Player 2 WIN!";
+                    MessageBox.Show(msgs);
+                    Application.Exit();
+
+                }
+            }
+        }
+    }
+}
